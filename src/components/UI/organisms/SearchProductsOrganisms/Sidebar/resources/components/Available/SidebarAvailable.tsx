14e57a7407afe00fac43890ac3562688a805c37c
Ali-HSNZ <Aliatraby@gmail.com>
2023-12-07 22:58:41 +0330

    feat: read available from query params in available section in search products page sidebar

--- src/components/UI/organisms/SearchProductsOrganisms/Sidebar/resources/components/Available/SidebarAvailable.tsx
+++ src/components/UI/organisms/SearchProductsOrganisms/Sidebar/resources/components/Available/SidebarAvailable.tsx
@@ -3,7 +3,9 @@ import { Checkbox } from '@mantine/core'
 
 import { type ISidebarAvailableProps } from './resources'
 
-const SidebarAvailable: FC<ISidebarAvailableProps> = ({ setQuery }) => {
+const SidebarAvailable: FC<ISidebarAvailableProps> = ({ setQuery, query }) => {
+    const isDefaultChecked: boolean = query && query === 'true' ? true : false
+
     const handleCheckbox = (status: boolean) => {
         setQuery({ available: status })
     }
@@ -11,6 +13,7 @@ const SidebarAvailable: FC<ISidebarAvailableProps> = ({ setQuery }) => {
     return (
         <section className='px-4 select-none'>
             <Checkbox
+                defaultChecked={isDefaultChecked}
                 onChange={(checkbox) => handleCheckbox(checkbox.target.checked)}
                 label='Display available products'
             />
--- src/components/UI/organisms/SearchProductsOrganisms/Sidebar/resources/components/Available/resources/types/type.ts
+++ src/components/UI/organisms/SearchProductsOrganisms/Sidebar/resources/components/Available/resources/types/type.ts
@@ -2,6 +2,7 @@ import { type TCriticalAny } from '@core/types/critical-any'
 
 interface ISidebarAvailableProps {
     setQuery: TCriticalAny
+    query: string | null | undefined
 }
 
 export default ISidebarAvailableProps
