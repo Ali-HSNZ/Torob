6d06320ee8526c181fe3cbc20a4807d782ccbfe6
Ali-HSNZ <Aliatraby@gmail.com>
2024-02-26 22:21:30 +0330

    style: responsive product detail section in product  page

--- src/components/UI/organisms/ProductDetailOrganisms/Detail/ProductDetail.tsx
+++ src/components/UI/organisms/ProductDetailOrganisms/Detail/ProductDetail.tsx
@@ -40,27 +40,27 @@ const ProductDetail: FC = () => {
                 </div>
 
                 <div className='flex flex-col gap-y-4'>
-                    <h1 className='font-semibold'>{STATIC_PRODUCT_DETAIL.title}</h1>
+                    <h1 className='font-semibold text-sm'>{STATIC_PRODUCT_DETAIL.title}</h1>
 
                     <div className='flex flex-col gap-y-4'>
                         <div className='text-sm flex items-center gap-x-1'>
                             <IconStarFilled className=' text-yellow-500' size={15} />
-                            <p className='font-semibold'>{STATIC_PRODUCT_DETAIL.rating.rate}</p>
+                            <p className='font-bold'>{STATIC_PRODUCT_DETAIL.rating.rate}</p>
                             <p className='text-gray-400'>
                                 ({Intl.NumberFormat('en-es').format(STATIC_PRODUCT_DETAIL.rating.count)})
                             </p>
                         </div>
 
-                        <div className='flex gap-x-4'>
-                            <div className='text-sm text-red-600 flex gap-x-1'>
-                                <p className='font-semibold'>
+                        <div className='flex gap-x-4 text-red-600'>
+                            <div className='text-sm flex gap-x-1'>
+                                <p className='font-bold'>
                                     {Intl.NumberFormat('en-es').format(STATIC_PRODUCT_DETAIL.comment)}
                                 </p>
                                 <p>comments</p>
                             </div>
 
-                            <div className='text-sm text-red-600 flex gap-x-1'>
-                                <p className='font-semibold'>
+                            <div className='text-sm flex gap-x-1'>
+                                <p className='font-bold'>
                                     {Intl.NumberFormat('en-es').format(STATIC_PRODUCT_DETAIL.question)}
                                 </p>
                                 <p>question</p>
@@ -69,12 +69,12 @@ const ProductDetail: FC = () => {
                     </div>
 
                     <div>
-                        <div className='flex font-semibold gap-x-1'>
-                            <p>color:</p>
-                            <p className='capitalize'>{selectedColor.title}</p>
+                        <div className='flex gap-x-1 text-sm'>
+                            <span className='text-sm text-gray-500'>color:</span>
+                            <p className='capitalize font-bold'>{selectedColor.title}</p>
                         </div>
 
-                        <div className='flex gap-x-3 mt-3'>
+                        <div className='flex gap-x-3 mt-2'>
                             {STATIC_PRODUCT_DETAIL.colors.map((color) => {
                                 return (
                                     <CActionIcon
@@ -82,10 +82,10 @@ const ProductDetail: FC = () => {
                                         onClick={() => setSelectedColor(color)}
                                         style={{ backgroundColor: color.hex }}
                                         key={color.id}
-                                        className={`w-9 cursor-pointer h-4 ring-2 z-[1] ring-offset-2 rounded-full ${
+                                        className={`w-9 cursor-pointer h-4 ring-2 z-[1] ring-offset-2 rounded-lg ${
                                             selectedColor.title === color.title ? 'ring-gray-400' : 'ring-gray-300'
                                         }`}
-                                    ></CActionIcon>
+                                    />
                                 )
                             })}
                         </div>
@@ -93,26 +93,25 @@ const ProductDetail: FC = () => {
 
                     <div className='flex gap-x-1 items-end'>
                         <span className='text-sm text-gray-500'>in</span>
-                        <span className='font-medium text-xl'>8</span>
+                        <span className='font-bold text-base'>8</span>
                         <span className='text-sm text-gray-500'>stores</span>
                     </div>
 
                     <div className='flex gap-x-1 items-end'>
                         <span className='text-sm text-gray-500'>from</span>
-                        <span className='font-medium text-lg'>
+                        <span className='font-bold text-base'>
                             ${Intl.NumberFormat('en-us').format(STATIC_PRODUCT_DETAIL.priceRange.min)}
                         </span>
-
                         <span className='text-sm text-gray-500'>to</span>
 
-                        <span className='font-medium text-lg'>
+                        <span className='font-bold text-base'>
                             ${Intl.NumberFormat('en-us').format(STATIC_PRODUCT_DETAIL.priceRange.max)}
                         </span>
                     </div>
                 </div>
             </div>
 
-            <p className='font-semibold mt-4 xl:mt-0'>Description:</p>
+            <p className='font-bold text-sm mt-4 xl:mt-0'>Description:</p>
             <span className='text-sm'>{STATIC_PRODUCT_DETAIL.description}</span>
         </section>
     )
