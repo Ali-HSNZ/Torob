af62126c98269913275e254be51e0843184f0223
Ali-HSNZ <Aliatraby@gmail.com>
2024-02-26 21:40:57 +0330

    style: responsive stores list section in product page

--- src/components/UI/organisms/ProductDetailOrganisms/Stores/ProductDetailStores.tsx
+++ src/components/UI/organisms/ProductDetailOrganisms/Stores/ProductDetailStores.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react'
 import { IconBuildingStore, IconTruckDelivery } from '@tabler/icons-react'
 import { IconShieldCheck } from '@tabler/icons-react'
+import { IconShoppingBag } from '@tabler/icons-react'
 
 import { CButton } from '@atoms/Button'
 
@@ -10,13 +11,16 @@ const ProductDetailStores: FC = () => {
             <p className='text-lg font-medium'>Stores</p>
             <ul className='flex flex-col gap-y-4 mt-4'>
                 {[1, 2, 3, 4].map((e) => (
-                    <li key={e} className='p-4 flex items-center gap-x-4 justify-between bg-white'>
-                        <div className='grid grow grid-cols-4 gap-x-4 justify-between items-center'>
-                            <div className='flex  gap-2'>
-                                <IconBuildingStore stroke={1} />
+                    <li
+                        key={e}
+                        className='p-4 flex flex-col sm:flex-row md:items-center gap-4 justify-between bg-white'
+                    >
+                        <div className='grid  w-full grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-between items-center'>
+                            <div className='flex gap-2'>
+                                <IconBuildingStore className='shrink-0' stroke={1} />
                                 <div className='flex flex-col gap-y-1 items-start'>
                                     <p className='font-medium text-lg'>Atlas</p>
-                                    <div className='flex gap-x-2'>
+                                    <div className='flex gap-x-1'>
                                         <div className='text-xs flex gap-x-1 font-medium text-gray-500'>
                                             <p className='text-yellow-600'>50%</p>
                                             <p>Satisfaction</p>
@@ -30,23 +34,21 @@ const ProductDetailStores: FC = () => {
                                 </div>
                             </div>
 
-                            <div className='flex bg-gradient-to-r from-white to-100% w-fit py-1 px-2 to-gray-100 rounded-r-full border-r border-gray-300 gap-x-1 text-sm items-center '>
+                            <div className='flex gap-2 underline underline-offset-[6px] text-sm h-full md:justify-end pb-1 xl:pb-0 xl:justify-start items-end xl:items-center '>
                                 <p>sent in 3 working days</p>
                                 <IconTruckDelivery stroke={1} />
                             </div>
 
-                            <div className='flex gap-x-2  items-center'>
+                            <div className='flex gap-x-2 md:col-span-2 xl:col-span-1 items-center'>
                                 <IconShieldCheck className='text-green-600 shrink-0' />
                                 <p className='text-sm font-medium '>
                                     Guarantee of authenticity and physical health of goods
                                 </p>
                             </div>
-
-                            <p className='text-center text-lg font-medium'>
-                                ${Intl.NumberFormat('en-us').format(654654)}
-                            </p>
                         </div>
-                        <CButton className='shrink-0'>Add to card </CButton>
+                        <CButton leftSection={<IconShoppingBag />} className='shrink-0'>
+                            ${Intl.NumberFormat('en-us').format(654654)}
+                        </CButton>
                     </li>
                 ))}
             </ul>
