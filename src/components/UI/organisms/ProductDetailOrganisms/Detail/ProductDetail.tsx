5f71768779aae0e13d428d17985b663fb1c5955c
Ali Hassanzadeh <aliatraby@gmail.com>
2024-03-10 14:43:22 +0330

    feat: create 'mony-back' section in product detail page

--- src/components/UI/organisms/ProductDetailOrganisms/Detail/ProductDetail.tsx
+++ src/components/UI/organisms/ProductDetailOrganisms/Detail/ProductDetail.tsx
@@ -3,7 +3,7 @@
 import { type FC, useState } from 'react'
 import Image from 'next/image'
 import { STATIC_PRODUCT_DETAIL } from '..'
-import { IconStarFilled } from '@tabler/icons-react'
+import { IconAlertCircleFilled, IconStarFilled } from '@tabler/icons-react'
 import { IconDots } from '@tabler/icons-react'
 
 import { CActionIcon } from '@atoms/ActionIcon'
@@ -82,8 +82,8 @@ const ProductDetail: FC = () => {
                                         onClick={() => setSelectedColor(color)}
                                         style={{ backgroundColor: color.hex }}
                                         key={color.id}
-                                        className={`w-9 cursor-pointer h-4 ring-2 z-[1] ring-offset-2 rounded-lg ${
-                                            selectedColor.title === color.title ? 'ring-gray-400' : 'ring-gray-300'
+                                        className={`w-9 cursor-pointer h-4 ring-2 z-[1] ring-offset-2  ${
+                                            selectedColor.title === color.title ? 'ring-gray-500' : 'ring-gray-300'
                                         }`}
                                     />
                                 )
@@ -91,7 +91,7 @@ const ProductDetail: FC = () => {
                         </div>
                     </div>
 
-                    <div className='flex gap-x-1 items-end'>
+                    <div className='flex gap-x-1 items-end '>
                         <span className='text-sm text-gray-500'>in</span>
                         <span className='font-bold text-base'>8</span>
                         <span className='text-sm text-gray-500'>stores</span>
@@ -111,8 +111,22 @@ const ProductDetail: FC = () => {
                 </div>
             </div>
 
-            <p className='font-bold text-sm mt-4 xl:mt-0'>Description:</p>
-            <span className='text-sm'>{STATIC_PRODUCT_DETAIL.description}</span>
+            <div>
+                <p className='font-bold text-sm mt-4 xl:mt-0'>Description:</p>
+                <span className='text-sm'>{STATIC_PRODUCT_DETAIL.description}</span>
+            </div>
+
+            <div className='mt-4 flex items-center bg-gray-100 p-1.5 rounded-lg gap-1.5'>
+                <IconAlertCircleFilled size={17} className='shrink-0 ' />
+                <div className='flex flex-col gap-y-1'>
+                    <p className='text-xs font-bold'>Mony-Back</p>
+                    <p className='text-xs text-gray-500'>
+                        The request to return the goods in the mouse group (consultation) with the reason of
+                        cancellation of the purchase can only be approved if the goods are in their original condition
+                        (if they are sealed, the goods must not have been opened).
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
