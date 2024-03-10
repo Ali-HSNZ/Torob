06c43e1c5c936680258daf209fe629486c012b38
Ali Hassanzadeh <aliatraby@gmail.com>
2024-03-10 13:54:26 +0330

    style: change section cols in cart page

--- src/components/UI/organisms/CartOrganisms/ShoppingCart/ShoppingCart.tsx
+++ src/components/UI/organisms/CartOrganisms/ShoppingCart/ShoppingCart.tsx
@@ -4,7 +4,7 @@ import { ShoppingCartItem, STATIC_SHOPPING_CART_DATA } from './resources'
 
 const ShoppingCart: FC = () => {
     return (
-        <div className='lg:col-span-2 bg-white border border-gray-100 rounded-l-xl overflow-hidden pb-4 px-4 flex overflow-y-auto h-full flex-col gap-4 flex-1'>
+        <div className='lg:col-span-3 bg-white border border-gray-100 rounded-l-xl overflow-hidden pb-4 px-4 flex overflow-y-auto h-full flex-col gap-4 flex-1'>
             <div className='flex flex-col z-10 pt-4 gap-4 sticky top-0 bg-white'>
                 <p className='text-base font-bold'>Shopping Cart</p>
                 <hr />
