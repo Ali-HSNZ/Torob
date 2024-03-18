851b57f92c5d4a9cf4aa4e095c605acbe8573c8b
Ali Hassanzadeh <aliatraby@gmail.com>
2024-03-18 14:10:52 +0330

    refactor: keen-slider molecule

--- src/components/UI/molecules/KeenSlider/keenSlider.tsx
+++ src/components/UI/molecules/KeenSlider/keenSlider.tsx
@@ -11,6 +11,7 @@ import { type IKeenSliderProps } from './resources'
 
 const CustomKeenSlider: FC<IKeenSliderProps> = ({ children, isHiddenArrow, activeSlide, setActiveSlide, ...res }) => {
     const [loaded, setLoaded] = useState(false)
+    const [currentSlide, setCurrentSlide] = useState<number>(0)
 
     // slider image height dependency
     const AdaptiveHeight = (slider: KeenSliderInstance) => {
@@ -30,6 +31,9 @@ const CustomKeenSlider: FC<IKeenSliderProps> = ({ children, isHiddenArrow, activ
             created: () => {
                 setLoaded(true)
             },
+            slideChanged(slider) {
+                if (!activeSlide) setCurrentSlide(slider.track.details.rel)
+            },
             ...res,
         },
         [AdaptiveHeight]
@@ -40,6 +44,27 @@ const CustomKeenSlider: FC<IKeenSliderProps> = ({ children, isHiddenArrow, activ
         activeSlide !== undefined && instanceRef.current?.moveToIdx(activeSlide)
     }, [activeSlide, instanceRef])
 
+    const handleArrowLeft = () => {
+        if (instanceRef.current) {
+            instanceRef.current.prev()
+            if (setActiveSlide) setActiveSlide(instanceRef.current?.track.details.rel - 1)
+            else setCurrentSlide(instanceRef.current?.track.details.rel - 1)
+        }
+    }
+
+    const handleArrowRight = () => {
+        if (instanceRef.current) {
+            instanceRef.current.next()
+            if (setActiveSlide) setActiveSlide(instanceRef.current?.track.details.rel + 1)
+            else setCurrentSlide(instanceRef.current?.track.details.rel + 1)
+        }
+    }
+
+    const isDisabledArrowLeft: boolean = (activeSlide ?? currentSlide) === 0
+
+    const isDisabledArrowRight =
+        instanceRef.current && (activeSlide ?? currentSlide) === instanceRef.current.track.details.slides.length - 1
+
     return (
         <section className='keen-slider relative' ref={sliderRef}>
             {/* sliders */}
@@ -48,39 +73,27 @@ const CustomKeenSlider: FC<IKeenSliderProps> = ({ children, isHiddenArrow, activ
             {/* Arrows */}
             {loaded && !isHiddenArrow && instanceRef.current && (
                 <>
-                    <div className={`absolute ${activeSlide === 0 ? 'hidden' : ''} left-4 top-1/2 -translate-y-1/2`}>
+                    <div className={`absolute ${isDisabledArrowLeft ? 'hidden' : ''} left-4 top-1/2 -translate-y-1/2`}>
                         <CActionIcon
-                            className='p-1.5 border border-gray-200 shadow-md rounded-full bg-white text-gray-800'
+                            className='p-1.5 border  border-gray-200 shadow-md rounded-full bg-white text-gray-800'
                             color='dark'
                             size={40}
-                            disabled={activeSlide === 0}
-                            onClick={() => {
-                                instanceRef.current?.prev()
-                                instanceRef.current &&
-                                    setActiveSlide &&
-                                    setActiveSlide(instanceRef.current?.track.details.rel - 1 || 0)
-                            }}
+                            disabled={isDisabledArrowLeft}
+                            onClick={handleArrowLeft}
                         >
                             <IconChevronLeft />
                         </CActionIcon>
                     </div>
 
                     <div
-                        className={`absolute right-4 top-1/2 -translate-y-1/2 ${
-                            activeSlide === instanceRef.current.track.details.slides.length - 1 ? 'hidden' : ''
-                        }`}
+                        className={`absolute right-4 top-1/2 -translate-y-1/2 ${isDisabledArrowRight ? 'hidden' : ''}`}
                     >
                         <CActionIcon
                             className='p-1.5 border border-gray-200 shadow-md rounded-full bg-white text-gray-800'
                             color='dark'
                             size={40}
-                            disabled={activeSlide === instanceRef.current.track.details.slides.length - 1}
-                            onClick={() => {
-                                instanceRef.current?.next()
-                                instanceRef.current &&
-                                    setActiveSlide &&
-                                    setActiveSlide(instanceRef.current?.track.details.rel + 1 || 0)
-                            }}
+                            disabled={Boolean(isDisabledArrowRight)}
+                            onClick={handleArrowRight}
                         >
                             <IconChevronRight />
                         </CActionIcon>
